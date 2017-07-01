b63f2147e017e22cc96f182201850f1bc9192ed1
Zane Mountcastle <zane.mountcastle@gmail.com>
2017-06-30 23:03:54 -0400

    Improved CLI

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ var mdns = require('multicast-dns')()
 var say = require('say');
 
 mdns.on('response', function(response) {
-  console.log('got a response packet:', response)
-  say.speak("Response " + service.name);
+  // console.log('got a response packet:', response)
+  for (i = 0; i<response.answers.length; i++) {
+    // say.speak("Response " + response.answers[i].data);
+    console.log("[Response] << " + response.answers[i].name);
+  }
 })
 
 mdns.on('query', function(query) {
-  console.log('got a query packet:', query)
-  say.speak("Query " + service.name);
+  // console.log('got a query packet:', query);
+  for (i = 0; i<query.questions.length; i++) {
+    // say.speak("Query " + query.questions[i].name);
+    console.log("[Query] >> " + query.questions[i].name);
+  }
 })
 
 // lets query for an A record for 'brunhilde.local'
@@ -18,3 +24,8 @@ mdns.query({
     type: 'AAAA'
   }]
 })
+
+process.on("SIGINT", () => {
+  mdns.destroy();
+  console.log("Caught SIGINT. Exiting...");
+});
