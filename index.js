577b2fd807190b482f11690c0b13652595575aa3
Zane Mountcastle <zane.mountcastle@gmail.com>
2017-06-30 22:13:10 -0400

    Changed MDNS package dependency

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-var mdns = require('mdns');
+var mdns = require('multicast-dns')()
 var say = require('say');
 
-// advertise a http server on port 4321
-var ad = mdns.createAdvertisement(mdns.tcp('http'), 4321);
-ad.start();
+mdns.on('response', function(response) {
+  console.log('got a response packet:', response)
+  say.speak("Response " + service.name);
+})
 
-// watch all http servers
-var browser = mdns.createBrowser(mdns.tcp('http'));
-browser.on('serviceUp', function(service) {
-  console.log("service up: ", service);
-  say.speak("Hello " + service.name);
-});
+mdns.on('query', function(query) {
+  console.log('got a query packet:', query)
+  say.speak("Query " + service.name);
+})
 
-
-browser.on('serviceDown', function(service) {
-  console.log("service down: ", service);
-});
-
-browser.start();
-
-// discover all available service types
-var all_the_types = mdns.browseThemAll(); // all_the_types is just another browser...
+// lets query for an A record for 'brunhilde.local'
+mdns.query({
+  questions:[{
+    name: 'Zanes-Iphone.local',
+    type: 'AAAA'
+  }]
+})
